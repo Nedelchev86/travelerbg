01b6256f950b55e723e918d27fd12ded1a0afa6f
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-10 22:52:05 +0200

    Add top destination component

---
+++ frontend/src/app/top-destination/top-destination.component.ts
@@ -0,0 +1,53 @@
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { SetBgImageDirective } from '../set-bg-image.directive';
+import { HttpClient } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
+import { LoaderComponent } from '../shared/loader/loader.component';
+
+@Component({
+  selector: 'app-top-destination',
+  standalone: true,
+  imports: [SetBgImageDirective, RouterLink, LoaderComponent, LoaderComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './top-destination.component.html',
+  styleUrl: './top-destination.component.css',
+})
+export class TopDestinationComponent implements OnInit {
+  loading = true;
+  http = inject(HttpClient);
+  top_destinations = Array<any>();
+  ngOnInit() {
+    this.http.get('http://localhost:8000/api/destinations/').subscribe({
+      next: (data: any) => {
+        this.top_destinations = data.results;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.log(err), (this.loading = false);
+        this.loading = false;
+      },
+    });
+  }
+  ngAfterViewInit() {
+    this.loadScript('assets/js/test.js');
+  }
+
+  loadScript(src: string) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = () => {
+      console.log('Script loaded successfully.');
+    };
+    script.onerror = () => {
+      console.error('Error loading script.');
+    };
+    document.body.appendChild(script);
+  }
+}
