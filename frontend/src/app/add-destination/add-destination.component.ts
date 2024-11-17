9349a62fa5cc1ee58071f03d88e1fb9966c9875f
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-17 16:01:02 +0200

    Add new functionality

--- frontend/src/app/add-destination/add-destination.component.ts
+++ frontend/src/app/add-destination/add-destination.component.ts
@@ -1,53 +1,87 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
+  AbstractControl,
+  FormArray,
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CloudinaryuploadService } from '../shared/services/cloudinaryupload.service';
+import {
+  MapGeocoder,
+  MapAdvancedMarker,
+  GoogleMap,
+} from '@angular/google-maps';
 
 @Component({
   selector: 'app-add-destination',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, GoogleMap, MapAdvancedMarker],
   templateUrl: './add-destination.component.html',
   styleUrl: './add-destination.component.css',
 })
 export class AddDestinationComponent {
   addDestinationForm: FormGroup;
   categories: any[] = [];
+  tags: FormArray;
 
+  center: google.maps.LatLngLiteral = { lat: 42.504792, lng: 27.462636 };
+  zoom = 15;
+  markerPosition: google.maps.LatLngLiteral = {
+    lat: 42.504792,
+    lng: 27.462636,
+  };
+  mapOptions: google.maps.MapOptions = {
+    mapId: '453204c6eedd332f',
+    gestureHandling: 'greedy',
+  };
+  geocoder = inject(MapGeocoder);
 
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private cloudinaryuploadService: CloudinaryuploadService
   ) {
     this.addDestinationForm = this.fb.group({
       title: [''],
-      tags: [''],
       category: [''],
       basic_information: [''],
       responsibilities: [''],
       benefits: [''],
-      image: [null, Validators.required],
-      image2: [null],
-      image3: [null],
-      image4: [null],
-      image5: [null],
-      image6: [null],
+      image: ['', Validators.required],
+      image2: [''],
+      image3: [''],
+      image4: [''],
+      image5: [''],
       vacancy: [''],
       location: [''],
       cost: [''],
+      lat: [''], // Add lat control
+      lng: [''], // Add lng control
       is_published: [true],
+      tags: this.fb.array([], this.minLengthArray(1)),
+      newTag: [''], // Input field for new tag
     });
+    this.tags = this.addDestinationForm.get('tags') as FormArray;
   }
 
   ngOnInit(): void {
     this.fetchCategories();
+  }
+
+  minLengthArray(min: number) {
+    return (c: AbstractControl): ValidationErrors | null => {
+      if (c instanceof FormArray && c.length >= min) {
+        return null;
+      }
+      return { minLengthArray: true };
+    };
   }
 
   fetchCategories(): void {
@@ -61,17 +95,62 @@ export class AddDestinationComponent {
     );
   }
 
-  onFileChange(event: any, field: string) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.addDestinationForm.patchValue({
-        [field]: file,
-      });
+  // onFileChange(event: any, field: string) {
+  //   if (event.target.files.length > 0) {
+  //     const file = event.target.files[0];
+  //     this.addDestinationForm.patchValue({
+  //       [field]: file,
+  //     });
 
+  //   }
+  // }
+  onFileChange(event: any, field: string): void {
+    console.log('changed');
+    const file = event.target.files[0];
+    if (file) {
+      this.cloudinaryuploadService.uploadImage(file).subscribe(
+        (response) => {
+          console.log('Image uploaded successfully:', response);
+          this.addDestinationForm.patchValue({
+            [field]: response.secure_url,
+          });
+        },
+        (error) => {
+          console.error('Error uploading image:', error);
+        }
+      );
     }
   }
 
+  onMapClick(event: google.maps.MapMouseEvent): void {
+    if (event.latLng) {
+      this.markerPosition = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      };
+      this.addDestinationForm.patchValue({
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      });
+    }
+  }
 
+  onLocationChange(): void {
+    const location = this.addDestinationForm.get('location')?.value;
+    if (location) {
+      this.geocodeAddress(location);
+    }
+  }
+
+  geocodeAddress(address: string): void {
+    this.geocoder.geocode({ address }).subscribe(({ results }) => {
+      if (results.length > 0) {
+        const location = results[0].geometry.location;
+        this.center = { lat: location.lat(), lng: location.lng() };
+        this.markerPosition = { lat: location.lat(), lng: location.lng() };
+      }
+    });
+  }
 
   // onSubmit() {
   //   const formData = new FormData();
@@ -91,6 +170,23 @@ export class AddDestinationComponent {
   //       }
   //     );
   // }
+  addNewTag(): void {
+    const newTag = this.addDestinationForm.get('newTag')?.value;
+    if (newTag) {
+      this.addTag(newTag);
+      this.addDestinationForm.get('newTag')?.reset();
+    }
+  }
+
+  addTag(tagName: string = ''): void {
+    if (tagName && !this.tags.value.includes(tagName)) {
+      this.tags.push(this.fb.control(tagName, Validators.required));
+    }
+  }
+
+  removeTag(index: number): void {
+    this.tags.removeAt(index);
+  }
 
   onSubmit() {
     if (this.addDestinationForm.invalid) {
@@ -100,7 +196,19 @@ export class AddDestinationComponent {
 
     const formData = new FormData();
     Object.keys(this.addDestinationForm.controls).forEach((key) => {
-      formData.append(key, this.addDestinationForm.get(key)?.value);
+      if (key === 'tags') {
+        const tags = this.tags.value;
+        tags.forEach((tag: string, index: number) => {
+          formData.append(`tags[${index}]`, tag);
+        });
+      } else if (key !== 'newTag') {
+        const controlValue = this.addDestinationForm.get(key)?.value;
+        if (controlValue instanceof File) {
+          formData.append(key, controlValue);
+        } else {
+          formData.append(key, controlValue ?? '');
+        }
+      }
     });
 
     this.http
--- frontend/src/app/edit-destination/edit-destination.component.ts
+++ frontend/src/app/edit-destination/edit-destination.component.ts
@@ -2,17 +2,25 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, Input, OnInit } from '@angular/core';
 import {
+  AbstractControl,
+  FormArray,
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
   Validators,
 } from '@angular/forms';
+import {
+  MapGeocoder,
+  GoogleMap,
+  MapAdvancedMarker,
+} from '@angular/google-maps';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-destination',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, GoogleMap, MapAdvancedMarker],
   templateUrl: './edit-destination.component.html',
   styleUrl: './edit-destination.component.css',
 })
@@ -20,7 +28,20 @@ export class EditDestinationComponent implements OnInit {
   @Input() id!: string;
   editDestinationForm: FormGroup;
   categories: any[] = [];
+  tags: FormArray;
   destinationId: string | null = null;
+
+  center: google.maps.LatLngLiteral = { lat: 42.504792, lng: 27.462636 };
+  zoom = 15;
+  markerPosition: google.maps.LatLngLiteral = {
+    lat: 42.504792,
+    lng: 27.462636,
+  };
+  mapOptions: google.maps.MapOptions = {
+    mapId: '453204c6eedd332f',
+    gestureHandling: 'greedy',
+  };
+  geocoder = inject(MapGeocoder);
 
   constructor(
     private fb: FormBuilder,
@@ -30,7 +51,7 @@ export class EditDestinationComponent implements OnInit {
   ) {
     this.editDestinationForm = this.fb.group({
       title: ['', Validators.required],
-      tags: ['', Validators.required],
+      tags: this.fb.array([], this.minLengthArray(1)),
       category: ['', Validators.required],
       basic_information: ['', Validators.required],
       responsibilities: ['', Validators.required],
@@ -41,11 +62,15 @@ export class EditDestinationComponent implements OnInit {
       image4: [null],
       image5: [null],
       image6: [null],
+      lat: [''],
+      lng: [''],
       vacancy: [''],
       location: ['', Validators.required],
       cost: [''],
+      newTag: [''], // Input field for new tag
       is_published: [true],
     });
+    this.tags = this.editDestinationForm.get('tags') as FormArray;
   }
 
   ngOnInit(): void {
@@ -55,7 +80,14 @@ export class EditDestinationComponent implements OnInit {
       this.getDestinationDetails(this.destinationId);
     }
   }
-
+  minLengthArray(min: number) {
+    return (c: AbstractControl): ValidationErrors | null => {
+      if (c instanceof FormArray && c.length >= min) {
+        return null;
+      }
+      return { minLengthArray: true };
+    };
+  }
   fetchCategories(): void {
     this.http.get('http://localhost:8000/api/categories/').subscribe(
       (response: any) => {
@@ -67,15 +99,84 @@ export class EditDestinationComponent implements OnInit {
     );
   }
 
-  getDestinationDetails(id: string): void {
-    this.http.get(`http://localhost:8000/api/destinations/${id}/`).subscribe(
-      (data: any) => {
-        this.editDestinationForm.patchValue(data);
-      },
-      (error) => {
-        console.error('Failed to fetch destination details', error);
+  getDestinationDetails(destinationId: string): void {
+    this.http
+      .get(`http://localhost:8000/api/destinations/${destinationId}/`)
+      .subscribe(
+        (response: any) => {
+          this.editDestinationForm.patchValue(response);
+          if (response.lat && response.lng) {
+            this.center = { lat: response.lat, lng: response.lng };
+            this.markerPosition = { lat: response.lat, lng: response.lng };
+          } else {
+            this.geocodeAddress(response.location);
+          }
+
+          response.tags.forEach((tag: { id: Number; name: string }) => {
+            this.tags.push(this.fb.control(tag.name, Validators.required));
+          });
+        },
+        (error) => {
+          console.error('Failed to fetch destination details', error);
+        }
+      );
+  }
+  //   this.http.get(`http://localhost:8000/api/destinations/${id}/`).subscribe(
+  //     (data: any) => {
+  //       this.editDestinationForm.patchValue(data);
+  //     },
+  //     (error) => {
+  //       console.error('Failed to fetch destination details', error);
+  //     }
+  //   );
+  // }
+
+  onMapClick(event: google.maps.MapMouseEvent): void {
+    if (event.latLng) {
+      this.markerPosition = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      };
+      this.editDestinationForm.patchValue({
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      });
+    }
+  }
+
+  onLocationChange(): void {
+    const location = this.editDestinationForm.get('location')?.value;
+    if (location) {
+      this.geocodeAddress(location);
+    }
+  }
+
+  geocodeAddress(address: string): void {
+    console.log('addres', address);
+    this.geocoder.geocode({ address }).subscribe(({ results }) => {
+      if (results.length > 0) {
+        const location = results[0].geometry.location;
+        this.center = { lat: location.lat(), lng: location.lng() };
+        this.markerPosition = { lat: location.lat(), lng: location.lng() };
       }
-    );
+    });
+  }
+  addNewTag(): void {
+    const newTag = this.editDestinationForm.get('newTag')?.value;
+    if (newTag) {
+      this.addTag(newTag);
+      this.editDestinationForm.get('newTag')?.reset();
+    }
+  }
+
+  addTag(tagName: string = ''): void {
+    if (tagName && !this.tags.value.includes(tagName)) {
+      this.tags.push(this.fb.control(tagName, Validators.required));
+    }
+  }
+
+  removeTag(index: number): void {
+    this.tags.removeAt(index);
   }
 
   onFileChange(event: any, field: string) {
@@ -95,36 +196,34 @@ export class EditDestinationComponent implements OnInit {
 
     const formData = new FormData();
     Object.keys(this.editDestinationForm.controls).forEach((key) => {
-      formData.append(key, this.editDestinationForm.get(key)?.value);
+      if (key === 'tags') {
+        const tags = this.tags.value;
+        tags.forEach((tag: string, index: number) => {
+          formData.append(`tags[${index}]`, tag);
+        });
+      } else if (key !== 'newTag') {
+        const controlValue = this.editDestinationForm.get(key)?.value;
+        if (controlValue instanceof File) {
+          formData.append(key, controlValue);
+        } else {
+          formData.append(key, controlValue ?? '');
+        }
+      }
     });
 
-    if (this.destinationId) {
-      this.http
-        .put(
-          `http://localhost:8000/api/destinations/${this.destinationId}/`,
-          formData
-        )
-        .subscribe(
-          (response) => {
-            console.log('Destination updated successfully', response);
-            this.router.navigate(['/profile/my-destinations']);
-          },
-          (error) => {
-            console.error('Failed to update destination', error);
-          }
-        );
-    } else {
-      this.http
-        .post('http://localhost:8000/api/destinations/', formData)
-        .subscribe(
-          (response) => {
-            console.log('Destination added successfully', response);
-            this.router.navigate(['/profile']);
-          },
-          (error) => {
-            console.error('Failed to add destination', error);
-          }
-        );
-    }
+    this.http
+      .put(
+        `http://localhost:8000/api/destinations/${this.destinationId}/`,
+        formData
+      )
+      .subscribe(
+        (response) => {
+          console.log('Destination updated successfully', response);
+          this.router.navigate(['/profile/my-destinations']);
+        },
+        (error) => {
+          console.error('Failed to update destination', error);
+        }
+      );
   }
 }
