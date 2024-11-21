d2fc6d07729f87850c19114b3a572215b474212a
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-21 19:34:13 +0200

    Add google map component

---
+++ frontend/src/app/google-map/google-map.component.ts
@@ -0,0 +1,71 @@
+import {
+  Component,
+  inject,
+  Input,
+  input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  MapGeocoder,
+  MapAdvancedMarker,
+  GoogleMap,
+} from '@angular/google-maps';
+
+@Component({
+  selector: 'app-google-map',
+  standalone: true,
+  imports: [GoogleMap, MapAdvancedMarker],
+  templateUrl: './google-map.component.html',
+  styleUrl: './google-map.component.css',
+})
+export class GoogleMapComponent implements OnChanges {
+  @Input() location: string = '';
+  @Input() lat: number = 42.504792;
+  @Input() lng: number = 27.462636;
+
+  center: google.maps.LatLngLiteral = { lat: this.lat, lng: this.lng };
+  zoom = 15;
+  markerPosition: google.maps.LatLngLiteral = {
+    lat: this.lat,
+    lng: this.lng,
+  };
+  mapOptions: google.maps.MapOptions = {
+    mapId: '453204c6eedd332f',
+    gestureHandling: 'greedy',
+  };
+
+  geocoder = inject(MapGeocoder);
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Lat:', this.lat, 'Lng:', this.lng);
+    if (changes['location'] && changes['location'].currentValue) {
+      console.log('ngOnChanges location:', this.location);
+      this.geocodeAddress(this.location);
+    }
+    if (
+      changes['lat'] &&
+      changes['lat'].currentValue &&
+      changes['lng'] &&
+      changes['lng'].currentValue
+    ) {
+      console.log('ngOnChanges lat:', this.lat, 'lng:', this.lng);
+      this.center = { lat: this.lat, lng: this.lng };
+      this.markerPosition = { lat: this.lat, lng: this.lng };
+    }
+  }
+
+  geocodeAddress(address: string): void {
+    this.geocoder.geocode({ address }).subscribe(({ results }) => {
+      if (results.length > 0) {
+        const location = results[0].geometry.location;
+
+        this.lat = location.lat();
+        this.lng = location.lng();
+        this.center = { lat: location.lat(), lng: location.lng() };
+        this.markerPosition = { lat: location.lat(), lng: location.lng() };
+      }
+    });
+  }
+}
