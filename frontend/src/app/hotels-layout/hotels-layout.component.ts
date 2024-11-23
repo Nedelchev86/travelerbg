51ddc2c4fa967094f4fb63aa95e63d258a2dba9c
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-23 20:46:42 +0200

    Add banner component and styles

--- frontend/src/app/hotels-layout/hotels-layout.component.ts
+++ frontend/src/app/hotels-layout/hotels-layout.component.ts
@@ -14,6 +14,7 @@ import { TopTravelersComponent } from '../top-travelers/top-travelers.component'
 import { TopDestinationComponent } from '../top-destination/top-destination.component';
 import { TopRatedDestinationsComponent } from '../top-rated-destinations/top-rated-destinations.component';
 import { HttpClient } from '@angular/common/http';
+import { BannerComponent } from "../shared/banner/banner.component";
 
 @Component({
   selector: 'app-hotels-layout',
@@ -29,7 +30,8 @@ import { HttpClient } from '@angular/common/http';
     TopRatedDestinationsComponent,
     RouterLink,
     RouterLinkActive,
-  ],
+    BannerComponent
+],
   templateUrl: './hotels-layout.component.html',
   styleUrl: './hotels-layout.component.css',
 })
