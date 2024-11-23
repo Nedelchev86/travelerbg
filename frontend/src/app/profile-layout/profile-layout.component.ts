d66419aec09afd3b2118a5a012c4e873224b9eae
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-23 20:49:20 +0200

    Add banner component

--- frontend/src/app/profile-layout/profile-layout.component.ts
+++ frontend/src/app/profile-layout/profile-layout.component.ts
@@ -1,11 +1,18 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { AuthService } from '../auth.service';
+import { SetBgImageDirective } from '../set-bg-image.directive';
+import { BannerComponent } from '../shared/banner/banner.component';
 
 @Component({
   selector: 'app-profile-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, BannerComponent],
   templateUrl: './profile-layout.component.html',
   styleUrl: './profile-layout.component.css',
 })
