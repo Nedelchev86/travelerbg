8e0e70457b455ce0cc6f8f39a553bf6ae520c17c
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-24 14:37:54 +0200

    Change BE admin fields

--- backend/backend/activities/admin.py
+++ backend/backend/activities/admin.py
@@ -5,12 +5,12 @@ from django.db import models
 # Register your models here.
 class ActivitiesAdmin(admin.ModelAdmin):
     # List display fields
-    list_display = ['title', 'user', 'price', 'time', 'category']
+    list_display = ['title', 'user', 'price', 'duration', 'category']
     list_filter = ['user', 'tags', 'price', 'category']
     search_fields = ['title', 'description']
 
     # Fields to be displayed in the form for adding/editing
-    fields = ['user', 'title', 'category', 'description', 'image', 'image2', 'image3', 'price', 'time', 'tags']
+    fields = ['user', 'title', 'category', 'description', 'highlights', 'image', 'image2', 'image3', 'price', 'duration', 'tags']
 
     # Widget for tags
     filter_horizontal = ('tags',)
