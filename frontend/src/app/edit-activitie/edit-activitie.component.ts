8a34253fd36c77b17aa63f0d871687e36901b5d9
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-24 14:42:39 +0200

    Add dit activities component

---
+++ frontend/src/app/edit-activitie/edit-activitie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-activitie',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-activitie.component.html',
+  styleUrl: './edit-activitie.component.css'
+})
+export class EditActivitieComponent {
+
+}
