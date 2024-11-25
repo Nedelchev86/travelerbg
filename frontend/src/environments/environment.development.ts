d6c6d2e876d9e80fe8d41d024b654efacd72a00f
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-25 10:48:41 +0200

    Make env

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://orca-app-x6eo8.ondigitalocean.app/api/',
+  };
+  
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://orca-app-x6eo8.ondigitalocean.app/api/',
+};
