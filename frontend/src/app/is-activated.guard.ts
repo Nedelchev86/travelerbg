fc6318a9adb3a7d29de22c67fabe0cf00d55663d
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-11-25 10:49:11 +0200

    Add isActivated guard

---
+++ frontend/src/app/is-activated.guard.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { ToastrService } from 'ngx-toastr';
+
+export const isActivatedGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const toastr = inject(ToastrService);
+
+  const currentUser = authService.isActivated();
+  console.log(currentUser);
+  if (currentUser) {
+    return true;
+  } else {
+    toastr.error(
+      'Your account is not activated. Please go to "Edit Profile',
+      'Access Denied'
+    );
+    router.navigate(['/profile']);
+    return false;
+  }
+};
