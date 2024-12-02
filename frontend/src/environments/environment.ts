a843a0780d6f856bcbd735a22f9a0ebc9b632e07
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-12-02 21:57:35 +0200

    Enviroment for render

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_BASE_UR'] || 'https://orca-app-x6eo8.ondigitalocean.app/api/',
-
+  apiUrl:
+    process.env['API_BASE_URL'] ||
+    'https://orca-app-x6eo8.ondigitalocean.app/api/',
 };
