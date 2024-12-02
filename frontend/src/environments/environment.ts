35ed2142689a4e180753a871999668fcd2e81ea0
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-12-02 22:23:13 +0200

    Enviroment for render

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    process.env['API_BASE_URL'] ||
-    'https://orca-app-x6eo8.ondigitalocean.app/api/',
+  apiUrl: 'https://travelerbe.onrender.com/api/',
 };
