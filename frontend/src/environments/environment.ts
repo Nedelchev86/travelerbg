fc8a15a91be67aa875c6e4d3602c6dc3f0cb7ef6
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-12-08 22:33:19 +0200

    Add url for DigitalOcean

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://orca-app-x6eo8.ondigitalocean.app/api/',
-  apiUrl: 'https://travelerbe.onrender.com/api/',
+  apiUrl: 'https://orca-app-x6eo8.ondigitalocean.app/api/',
 };
