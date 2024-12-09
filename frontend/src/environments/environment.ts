fcfbcc8e5c3b294dd847aea9e9d23fd061eb781c
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-12-09 22:01:34 +0200

    Add onDestroy stop api call

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://orca-app-x6eo8.ondigitalocean.app/api/',
+  // apiUrl: 'https://orca-app-x6eo8.ondigitalocean.app/api/',
+  apiUrl: 'https://travelerbe.onrender.com/api/',
 };
