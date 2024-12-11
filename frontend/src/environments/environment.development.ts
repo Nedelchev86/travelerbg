5d70994318935fc5c9949d206747d4aa99b91336
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-12-11 20:19:53 +0200

    Change delete modal style

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://orca-app-x6eo8.ondigitalocean.app/api/',
-  apiUrl: 'http://localhost:8000/api/',
+  apiUrl: 'https://orca-app-x6eo8.ondigitalocean.app/api/',
+  // apiUrl: 'http://localhost:8000/api/',
 };
